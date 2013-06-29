743b63421bb3c665655594ea68b0e8154a2cecbe
JP DeVries <johnpaul.devries@gmail.com>
2013-06-29 00:22:50 -0700

    Clarifying Docs

--- core/components/hits/elements/snippets/snippet.hits.php
+++ core/components/hits/elements/snippets/snippet.hits.php
@@ -12,8 +12,8 @@
 		Get a comma seperated list of ids of the 10 most visited pages 10 levels down from the web context
 		[[!Hits? &parents=`0` &depth=`10` &limit=`10` &outSeperator=`,` &chunk=`hitID`]]
 
-		Get a comma seperated list of ids of the 4 least visited pages that are children of resource 4
-		[[!Hits? &parents=`4` limit=`4` &dir=`ASC`  &outSeperator=`,` &chunk=`hitID`]]
+		Get a comma seperated list of ids of the 4 least visited pages that are children of resource 2
+		[[!Hits? &parents=`2` limit=`4` &dir=`ASC`  &outSeperator=`,` &chunk=`hitID`]]
 
 		Record a hit for resource 3
 		[[!Hits? &punch=`3`]]
